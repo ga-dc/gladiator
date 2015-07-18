a56a2ad098761560ba7b87363e5161afc4e0caf4
Edward Park <parkes.edward@gmail.com>
2015-07-18 12:32:10 -0400

    added initialize and name and weapon method

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,16 @@
 class Gladiator
+
+  def initialize(new_name, new_weapon)
+    @name = new_name
+    @weapon = new_weapon
+  end
+
+  def name
+    return @name
+  end
+
+  def weapon
+    return @weapon
+  end
+
 end
