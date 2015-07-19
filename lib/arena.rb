012875a5798963193140b8d1f52c1ff289fb5005
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-19 19:11:03 -0400

    Passed all tests

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,49 @@
 class Arena
+  attr_accessor :name, :gladiators
+
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+  def add_gladiator(gladiator)
+      gladiators.push(gladiator)
+      if gladiators.length > 2
+        gladiators.pop
+      end
+  end
+  def fight()
+    if gladiators.length == 2
+      weapon_one = gladiators.first.get_weapon
+      weapon_two = gladiators.last.get_weapon
+      if weapon_one == weapon_two
+        #a tie, both are expelled
+        gladiators.pop
+        gladiators.pop
+      elsif weapon_one == "Spear"
+        if weapon_two == "Trident"
+          #Trident Wins #Gladiator 1 is expelled
+          gladiators.delete_at(0)
+        elsif weapon_two == "Club"
+          #Spear wins. Gladiator 2 is expelled
+          gladiators.delete_at(1)
+        end
+      elsif weapon_one == "Trident"
+        if weapon_two == "Spear"
+          #Trident wins. Gladiator 2 is expelled
+          gladiators.delete_at(1)
+        elsif weapon_two == "Club"
+          #Club wins. Gladiator 1 is expelled
+          gladiators.delete_at(0)
+        end
+      elsif weapon_one == "Club"
+        if weapon_two == "Spear"
+          #Spear wins. Gladiator 1 is expelled
+          gladiators.delete_at(0)
+        elsif weapon_two == "Trident"
+          #Club wins. Gladiator 2 is expelled
+          gladiators.delete_at(1)
+        end
+      end #closes if weapon one
+    end #closes if gladiators length is two
+  end #closes the fight
 end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,12 @@
 class Gladiator
+  attr_accessor :name, :weapon
+
+  def initialize(init_name, init_weapon)
+    @name = init_name
+    @weapon = init_weapon
+  end
+
+  def get_weapon()
+    return @weapon 
+  end
 end
