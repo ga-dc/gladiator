c0b2899258a9302378efe250e8d33b151d0c4f0e
Laura Nawrocki <lwn2@aol.com>
2015-07-19 22:06:46 -0400

    def of name and weapon

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,17 @@
 class Arena
-end
+
+  def initialize(name)
+    @name = name
+    @gladiators = []
+  end
+
+  def name
+    return @name.capitalize
+  end
+
+  def gladiators
+    return @gladiators
+  end
+
+
+end #end class Arena
