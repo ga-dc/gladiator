243b00572e0e6301722ee8c91df904116852bb96
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-20 13:13:51 -0400

    resolve gladiator name and weapon errors

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,16 @@
 class Gladiator
+  def initialize(gladiator_name, gladiator_weapon)
+   @name = gladiator_name
+   @weapon = gladiator_weapon
+  end
+
+  def name
+    return @name
+  end
+
+  def weapon
+    return @weapon
+  end
+
+
 end
