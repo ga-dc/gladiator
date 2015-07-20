efc0fb9f57099ec1976305847ca707441f99d908
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-20 08:49:11 -0400

    added first half of tests

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,15 @@
 class Arena
+  attr_reader :name, :gladiators, :add_gladiator
+
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators  = []
+  end
+
+  def add_gladiator(new_gladiator)
+    if @gladiators.count < 2
+      @gladiators.push(new_gladiator)
+    end
+  end
+
 end
