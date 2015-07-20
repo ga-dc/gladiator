e22b3b84c5efb4bbe2c81cc2b30d474e4435ce50
Lisa <lscottcook@hotmail.com>
2015-07-19 22:41:36 -0400

    added to wrong branch

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,35 @@
 class Arena
+def initialize(name)
+   @name= name.capitalize
+   @gladiators= []
+  end
+
+ def name
+ return @name
+ end
+
+ def gladiators
+   return @gladiators
+
+ end
+
+#add until 2
+
+def add_gladiator(gladiator)
+  @gladiators.push(gladiator)
+  until (@gladiators == 2)
+    retutn (@gladiators)
+
 end
+
+end #end of class
+
+
+
+
+#if == 2 fight and eliminate losing
+#assign a weapon
+#Trident > Spear
+# Spear > Club
+# Club > Trident
+# weapon == weapon eliminate
