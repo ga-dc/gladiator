c53f53c46613bc710c9a58ab8f4f30b3484803e6
Jeff Jones <jeff.jones1@gmail.com>
2015-07-20 19:11:54 -0400

    initial set up to fight committed for gladiators

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,14 @@
 class Arena
+  attr_accessor :name
+  attr_accessor :gladiators
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+  def add_gladiator(person)
+    @gladiators.push(person) if @gladiators.length < 2
+  end
+  def fight
+     
+  end
 end
