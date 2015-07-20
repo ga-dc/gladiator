aad89969f1b46c62e66398c0566775bde5e88a26
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-19 20:30:34 -0400

    basic gladiator and area class set up

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,33 @@
 class Arena
-end
+
+  def initialize (name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+
+    def name
+      return @name
+    end
+
+    def gladiators
+      return @gladiators
+    end
+
+    #Add a gladiator to the arena and push it to the gladiators array if there are less than two gladiators in the arena
+      def add_gladiator(new_gladiator)
+        if gladiators.count < 2
+        @gladiators.push(new.gladiator)
+      end
+
+end #of class Arena
+
+
+
+#The arena should never have more than 2 gladiators in it at a time
+        #If the number of gladiators = 2, fight #loop? and eliminate
+
+        #assign a weapon
+        #Trident > Spear
+        # Spear > Club
+        # Club > Trident
+        # weapon == weapon eliminate
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,18 @@
 class Gladiator
-end
+
+  #A gladiator has a name and a weapon
+
+  def initialize (name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+
+    def name
+      return @name
+    end
+
+    def weapon
+      return @weapon
+    end
+
+end #of class
