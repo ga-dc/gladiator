dea61d90794c5e26cdc418ff2f1acaf7277deed9
Robert <i.am.robert.benjamin@gmail.com>
2015-07-19 23:27:54 -0400

    Add arena name and gladiator array
    Also added the ability to add gladiators.

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,14 @@
 class Arena
+
+  attr_accessor :name, :gladiators
+
+  def initialize(arena_name)
+    @name = arena_name.capitalize
+    @gladiators = []
+  end
+
+  def add_gladiator(gladiator)
+    @gladiators.push(gladiator)
+  end
+
 end
