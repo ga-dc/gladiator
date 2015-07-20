7886c0926720d0f4f5f76277c11f325e188fa411
Rachel Warren <raelwa@gmail.com>
2015-07-20 08:22:32 -0400

    the gladiators are trying to get in to the arena.

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,48 @@
 class Arena
+
+  def initialize (name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+
+  def name
+    return @name
+  end
+
+  def gladiators
+    return @gladiators
+  end
+
+  def add_gladiator(gladiator)
+    @gladiators.push(gladiator)
+  end
+
+arena.add_gladiator("maximus")
+
 end
+
+# pbj = arena.new("PBJ")
+
+
+
+
+# rspec ./spec/arena_spec.rb:27 # Arena#gladiators starts empty
+
+
+# rspec ./spec/arena_spec.rb:34 # Arena#add_gladiator adds a gladiator to the arena
+
+
+# rspec ./spec/arena_spec.rb:38 # Arena#add_gladiator can add multiple gladiators
+
+
+# rspec ./spec/arena_spec.rb:43 # Arena#add_gladiator does not have more than two gladiators at once
+
+
+# rspec ./spec/arena_spec.rb:54 # Arena#fight when there are no gladiators does nothing
+
+
+# rspec ./spec/arena_spec.rb:61 # Arena#fight when there is one gladiator does nothing
+# rspec ./spec/arena_spec.rb:69 # Arena#fight when Spear v Trident kills: spear when fighting trident
+# rspec ./spec/arena_spec.rb:79 # Arena#fight when Club v Spear kills: club when fighting spear
+# rspec ./spec/arena_spec.rb:89 # Arena#fight when Trident v Club kills: trident when fighting club
+# rspec ./spec/arena_spec.rb:99 # Arena#fight when same Weapons kills: both if their weapons are the same
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -14,23 +14,5 @@ def weapon
 end
 
 
+
 end
-
-
-# 13) Gladiator#name has a name
-#     Failure/Error: subject(:gladiator){Gladiator.new("Maximus","Sword")}
-#     ArgumentError:
-#       wrong number of arguments (2 for 0)
-#     # ./spec/gladiator_spec.rb:6:in `initialize'
-#     # ./spec/gladiator_spec.rb:6:in `new'
-#     # ./spec/gladiator_spec.rb:6:in `block (2 levels) in <top (required)>'
-#     # ./spec/gladiator_spec.rb:10:in `block (3 levels) in <top (required)>'
-#
-# 14) Gladiator#name has a weapon
-#     Failure/Error: subject(:gladiator){Gladiator.new("Maximus","Sword")}
-#     ArgumentError:
-#       wrong number of arguments (2 for 0)
-#     # ./spec/gladiator_spec.rb:6:in `initialize'
-#     # ./spec/gladiator_spec.rb:6:in `new'
-#     # ./spec/gladiator_spec.rb:6:in `block (2 levels) in <top (required)>'
-#     # ./spec/gladiator_spec.rb:14:in `block (3 levels) in <top (required)>'
