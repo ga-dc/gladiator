87b93d7bc27818fed79f034d6a0286249606a6e9
Preston Davies <pdavies88@gmail.com>
2015-07-22 00:39:57 -0400

    all tests passed for gladiator exercise

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,37 @@
 class Arena
+  def initialize (name)
+    @name = name
+    @gladiators = []
+  end
+
+  def name
+    return @name.capitalize
+  end
+
+  def gladiators
+    return @gladiators
+  end
+
+  def add_gladiator(new_gladiator)
+    if @gladiators.length == 2
+    else
+    @gladiators.push(new_gladiator)
+  end
+end
+
+  def fight
+    if @gladiators.length == 2
+      if @gladiators[0].weapon == @gladiators[1].weapon
+        @gladiators =[]
+      elsif @gladiators[0].weapon == "Trident" && @gladiators[1].weapon == "Spear"
+        @gladiators.pop()
+      elsif @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Club"
+        @gladiators.pop()
+      elsif @gladiators[0].weapon == "Club" && @gladiators[1].weapon == "Trident"
+        @gladiators.pop()
+      else
+        @gladiators.shift()
+      end
+    end
+  end
 end
