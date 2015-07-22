c044db7539f497f77ce7d8a670e277b7235888c3
Ryan Kahle <jrkahle@hotmail.com>
2015-07-22 18:05:26 -0400

    All tests pass

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,41 @@
 class Arena
+
+  attr_accessor :arena_name, :gladiators
+
+  def initialize(name)
+    @arena_name = name
+    @gladiators = []
+  end
+
+  def name
+    @arena_name = @arena_name.split.map(&:capitalize).join(' ')
+  end
+
+  def add_gladiator(gladiator)
+    if @gladiators.length < 2
+      @gladiators.push(gladiator)
+    end
+  end
+
+  def fight
+     if @gladiators.length < 2
+       return
+     end
+     if @gladiators[0].weapon == @gladiators[1].weapon
+       return @gladiators = []
+     end
+
+     if @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Trident"
+       @gladiators.shift
+     elsif @gladiators[0].weapon == "Spear" && @gladiators[1].weapon == "Club"
+      @gladiators.pop
+     elsif @gladiators[0].weapon == "Trident" && @gladiators[1].weapon == "Club"
+      @gladiators.shift
+    end
+
+  end
+
 end
+
+
+# arena = Arena.new("megapolis")
