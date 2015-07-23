8002d297f64484ee0ad9519bd1ab6f51dd2a9ce5
Christine Lovett <clovett07@gmail.com>
2015-07-22 21:51:40 -0400

    added gladiator

--- spec/gladiator_spec.rb
+++ spec/gladiator_spec.rb
@@ -6,11 +6,11 @@ describe Gladiator do
   subject(:gladiator){Gladiator.new("Maximus","Sword")}
 
   describe "#name" do
-    it "has a name" do
+    if "has a name" do
       expect(gladiator.name).to_not be_nil
       expect(gladiator.name).to eq("Maximus")
     end
-    it "has a weapon" do
+    if "has a weapon" do
       expect(gladiator.weapon).to_not be_nil
       expect(gladiator.weapon).to eq("Sword")
     end
