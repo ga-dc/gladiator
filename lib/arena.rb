e089de0e804034bc088a1064663bba0ed7b6fd1e
Samuel Key <s.key08@gmail.com>
2015-07-23 07:24:42 -0400

    homework

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,36 @@
 class Arena
+  def initialize(name)
+    @name = name
+    @gladiators = []
+  end
+
+  def name
+    return @name.capitalize
+  end
+
+  def gladiators
+    return @gladiators
+  end
+
+  def add_gladiator(new_gladiator)
+    if @gladiators.count < 2
+      @gladiators << new_gladiator
+    end
+  end
+
+  def fight
+    weapon_switch = {"Trident" => "Spear","Spear" => "Club", "Club" => "Trident"}
+
+    if @gladiators.count == 2
+      if @gladiators.first.weapon == @gladiators.last.weapon
+        @gladiators.pop(2)
+      elsif weapon_switch[@gladiators.first.weapon] ==@gladiators.last.weapon
+        @gladiators.delete_at(1)
+      else
+        @gladiators.delete_at(0)
+      end
+    end
+  end
+
+
 end
