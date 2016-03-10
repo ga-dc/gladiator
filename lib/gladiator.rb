99bab7243096abed1b11e8a3e0bea7f2af0e00ee
Hilary Masland <hilarymasland@gmail.com>
2016-03-10 10:57:50 -0500

    Initial commit - building gladiator

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,19 @@
+# A gladiator has a name
+# A gladiator has a weapon (one of Spear, Club, Trident)
+require "pry"
+
 class Gladiator
+
+  # attr_accessor = :gladiator
+  # attr_accessor = :weapon
+
+
+  # def name
+  #   if Gladiator.name("Maximus")
+  #     return
+def initialize(initial_name, initial_weapon)
+  @name = initial_name
+  @weapon = initial_weapon
+
+
 end
