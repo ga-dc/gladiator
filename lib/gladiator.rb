0bf011f6b4caa30edb0f2cdbf60fcbbadda5fc49
winishasmith <winishasmith@gmail.com>
2016-03-10 09:57:42 -0500

    Gladiator has name and weapon

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,10 @@
 class Gladiator
+attr_accessor :name
+attr_accessor :weapon
+
+  def initialize(name, weapon)
+    @name = name
+    @weapon = weapon #should include Spear, Club, Trident
+  end
+
 end
