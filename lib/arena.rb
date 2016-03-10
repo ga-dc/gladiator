babc7a1cc41e401cca7670d189ac9e214b43e7fc
Toni Langley <chibia2@gmail.com>
2016-03-10 10:45:26 -0500

    add gladiators to arena

--- lib/arena.rb
+++ lib/arena.rb
@@ -2,15 +2,22 @@ class Arena
 
   #attr_accesor to have access to the read and write properties
   attr_accessor :name
+  attr_accessor :gladiators
 
   #initialize Arena
   def initialize(name)
+
+    #makes the 1st letter of the word capital
     @name = name.capitalize
+
+    #establishing gladiators array
+    @gladiators = []
   end
 
-  #establishing gladiators array
-  def gladiators
-    gladiators = []
+  def add_gladiator(gladiator)
+
+    #pushes the gladiator into the gladiators array
+    @gladiators << gladiator
   end
 
 end
