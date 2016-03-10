d92242fd5698d48bba1c3dab4495f2adbcb0dab9
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-10 15:28:56 -0500

    completed fight tests

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,34 @@
+
+
 class Arena
+  attr_accessor :name, :gladiators
+  # @@count_glads = 0
+
+  def initialize (name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+
+  def add_gladiator (gladiator)
+    if @gladiators.count < 2
+      @gladiators << gladiator
+    end
+  end
+
+  def fight
+    if @gladiators.count == 2
+      if @gladiators[0].weapon == ("Spear") && @gladiators[1].weapon == ("Trident")
+        @gladiators.delete_at(0)
+      elsif
+        @gladiators[0].weapon == ("Spear") && @gladiators[1].weapon == ("Club")
+        @gladiators.delete_at(1)
+      elsif
+        @gladiators[0].weapon == ("Trident") && @gladiators[1].weapon == ("Club")
+        @gladiators.delete_at(0)
+      elsif
+        @gladiators[0].weapon == @gladiators[1].weapon
+        @gladiators.clear
+      end
+    end
+  end
 end
