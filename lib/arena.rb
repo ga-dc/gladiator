3fdb69a17878c22d5e1636d67bd691fbb64c4dcd
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-10 10:22:32 -0500

    Arena name is always capitalized.

--- lib/arena.rb
+++ lib/arena.rb
@@ -3,7 +3,7 @@ class Arena
   attr_accessor :name         #This does not seem to work if there is a "_" in the variable.  I think this is because the arena_spec.rb file uses the syntax arena.name, so the instance variable has to be "name".
 
   def initialize(arena_name)
-    @name = arena_name
+    @name = arena_name.capitalize  #You don't necessarily need to create a new method to solve each rspec failure.  For example, I just added the capitalize method to arean_name.
   end
 
 end
