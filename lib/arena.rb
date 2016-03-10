b2a9b299a25923d1a6873260a1bac89892f672bf
Christine Movius <christine.movius@gmail.com>
2016-03-10 09:49:41 -0500

    add_gladiator tests pass

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,7 +1,17 @@
 class Arena
   attr_accessor :name, :gladiators
+
   def initialize name
     @name = name.capitalize
     @gladiators = []
   end
+
+  def add_gladiator gladiator
+    if @gladiators.length < 2
+      @gladiators << gladiator
+    else
+      puts "There are 2 gladiators in the arena already."
+    end
+  end
+
 end
