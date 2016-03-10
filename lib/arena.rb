58728d2fc0864d84785fff1c17d2d29d0a86c733
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-10 12:24:07 -0500

    No more than two gladiators in arena

--- lib/arena.rb
+++ lib/arena.rb
@@ -7,7 +7,12 @@ class Arena
   end
 
 def add_gladiator new_gladiator_name
+  if @gladiators.count ==2
+    puts "You cannot have more than two Gladiators in the arena"
+  else
   @gladiators << new_gladiator_name
+puts "#{@gladiators.count}"
+end
 end
 
 
