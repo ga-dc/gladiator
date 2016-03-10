f2adbc4ac202e09280054809d9288064e41c310e
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-10 16:21:28 -0500

    fixed Gladiator class

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,18 +1,7 @@
-# require "pry"
-
-
 class Gladiator
   attr_accessor :name, :weapon
-
   def initialize(name, weapon)
     @name = name
     @weapon = weapon
   end
-
-
-
 end
-
-
-# binding.pry
-# puts "ayyy"
