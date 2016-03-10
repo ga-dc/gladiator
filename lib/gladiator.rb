06c86e4413acf44d5689a742d4e13a0b0a046d95
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-10 10:39:48 -0500

    Glaidiator "has name" and "has weapon"

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,19 @@
 class Gladiator
+  attr_accessor :name, :weapon 
+
+  def initialize (name, weapon)
+    @name = name
+    @weapon = weapon
+    # @gladiator = gladiator
+  end
+#
+#   def gladiator (@name, weapon)
+#
+#   end
+#
+#   def weapon.allow =
+#     [Spear, Club, Trident, Sword, Chains]
+#
+#
+#   end
 end
