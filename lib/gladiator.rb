9f83d92ce77ed23b61f8c8c6c414d4d09ba0efd2
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-10 09:57:08 -0500

    checks weapon

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,7 +1,13 @@
+require 'pry'
+
 class Gladiator
-  attr_accessor :name, :weapon
+  attr_accessor :name, :weapon, :ok_weapons
     def initialize(name, weapon)
       @name = name
       @weapon = weapon
+      @ok_weapons = ["Spear","Trident","Club"]
     end
 end
+
+test = Gladiator.new('Mountain','Spear')
+binding.pry
