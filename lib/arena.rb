fd147544a0895777d24364509a8bb34bf0bdafd2
jblakeman <blakeman.john@gmail.com>
2016-03-10 09:46:53 -0500

    Add gladiator initializer, gladiators to arena

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,9 +1,17 @@
 class Arena
+    attr_accessor :gladiators
+
     def initialize name
         @name = name
+        @gladiators = []
     end
 
     def name
         @name.capitalize
     end
+
+    def add_gladiator gladiator
+        gladiators.push(gladiator) if gladiators.size < 2
+    end
+
 end
