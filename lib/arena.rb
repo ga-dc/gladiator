c6495c2b4c9c8ffffc0b16560388c688fc0b00d8
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-10 15:33:20 -0500

    it all works! commented out part is another way I had started trying and want to continue working on to make code clearner

--- lib/arena.rb
+++ lib/arena.rb
@@ -4,22 +4,18 @@ class Arena
   def initialize name, gladiators = []
     @name = name.capitalize
     @gladiators = gladiators
-  end
+    @fight = fight
 
-  # def announce_gladiators
-  #   @gladiators.each do |gladiator|
-  #     print "Introducing #{gladiator.name} to the Arena with a #{gladiator.weapon}."
-  #   end
-  # end
+  end
 
   def add_gladiator gladiator
     if @gladiators.count < 2
       @gladiators << gladiator
+      # puts "Welcome #{gladiator.name} to the arena, ready to fight with a #{gladiator.weapon}"
     else
     if @gladiators.count > 2
       remove_gladiators
       puts "You already have 2 gladiators fighting. Remove one!"
-      @gladiators.pop
     end
     end
   end
@@ -27,4 +23,38 @@ class Arena
   def remove_gladiators
     @gladiators.pop
   end
-end
+
+  def fight
+    if @gladiators.count == 2
+      if @gladiators[0].weapon == ("Spear") && @gladiators[1].weapon == ("Trident")
+        @gladiators.delete_at(0)
+      elsif @gladiators[0].weapon == ("Spear") && @gladiators[1].weapon == ("Club")
+        @gladiators.delete_at(1)
+      elsif @gladiators[0].weapon == ("Trident") && @gladiators[1].weapon == ("Club")
+         @gladiators.delete_at(0)
+      elsif @gladiators[0].weapon ==  @gladiators[1].weapon
+          @gladiators.clear
+      end
+      # end
+      end
+    end
+  end
+
+#   class Fight
+#     attr_accessor :winner
+#
+#     def initialize gladiator1, gladiator2
+#       if gladiator1.name == "Maximus"
+#         @winner = gladiator1
+#       elsif gladiator2.name == "Maximus"
+#         @winner = gladiator2
+#       elsif test [gladiator1.weapon, gladiator2.weapon]
+#         @winner = gladiator1
+#       elsif test [gladiatorB.weapon, gladiatorA.weapon]
+#         @winner = gladiator2
+#       else
+#         @winner = false
+#       end
+#     end
+#   end
+# end
