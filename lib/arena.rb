48aaeaeef2dc44d4e639ce2806681c487a709432
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-10 11:54:05 -0500

    Only 10 failures

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,22 @@
 class Arena
+    attr_accessor :name
+
+    def initialize (name)
+      @name = name.capitalize
+      @gladiator = []
+    end
+
+    def gladiator (name, weapon)
+      @gladiator = []
+    end
+
+    def add_gladiator (name, weapon)
+      add_gladiator = []
+    end
+
+  def fight()
+    fight = fight
+  # when
+  end
+
 end
