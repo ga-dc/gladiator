bf3b654c78e4d8eac79c5c42834f1d8607d289e1
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-10 09:22:40 -0500

    gladiator tests pass

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,11 @@
 class Gladiator
+  attr_accessor :name, :weapon
+
+  def initialize(initial_name, initial_weapon = "Sword")
+    @name = initial_name
+    @weapon = initial_weapon
+  end
+
+  def
+
 end
