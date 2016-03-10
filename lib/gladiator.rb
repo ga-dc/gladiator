27b9861aeee2f3fd7b1d663b64ac84210f97a8db
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-10 09:37:24 -0500

    passed gladiator name and weapon tests

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,11 @@
 class Gladiator
+
+  attr_reader :name
+  attr_reader :weapon
+
+  def initialize(gladiator_name, gladiator_weapon)
+    @name = gladiator_name
+    @weapon = gladiator_weapon
+  end
+  
 end
