9f48fdd7a5332626f949edc607c0b73bcce9c351
Patrick Moran <patrick_moran@mac.com>
2016-03-10 09:47:32 -0500

    gladiators not named maximus can only have one of the accepted weapons

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -3,9 +3,23 @@ class Gladiator
 
   def initialize(name, weapon)
     @name = name.capitalize
-    @weapon = weapon
+    @weapon = weapon.capitalize
+    check_weapon
   end
 
-  
+  def check_weapon
+    accepted_weapons = ["Spear", "Club", "Trident"]
+
+    unless name == "Maximus"
+      if accepted_weapons.include?(weapon)
+        return true
+      else
+        puts "The only accepted weapons are 'Spear', 'Club', and 'Trident'"
+        return false
+      end
+    end
+  end
 
 end
+
+gladiator = Gladiator.new("Patrick", "trident")
--- spec/gladiator_spec.rb
+++ spec/gladiator_spec.rb
@@ -4,6 +4,8 @@ require_relative '../lib/gladiator'
 describe Gladiator do
 
   subject(:gladiator){Gladiator.new("Maximus","Sword")}
+  subject(:gladiator2){Gladiator.new("Sanjaya", "Trident")}
+  subject(:gladiator3){Gladiator.new("Patrick", "Gun")}
 
   describe "#name" do
     it "has a name" do
@@ -13,6 +15,22 @@ describe Gladiator do
     it "has a weapon" do
       expect(gladiator.weapon).to_not be_nil
       expect(gladiator.weapon).to eq("Sword")
+    end
+
+    describe "#check_weapon" do
+      context "Unless gladiator name is maximus" do
+        it "has an accepted weapon" do
+          expect(gladiator.check_weapon).to be(nil)
+        end
+      end
+      context "if gladiator name is not maximus" do
+        it "has an accepted weapon" do
+          expect(gladiator2.check_weapon).to be(true)
+        end
+        it "does not have an accepted weapon" do
+          expect(gladiator3.check_weapon).to be(false)
+        end
+      end
     end
   end
 end
