1975152d0d211135a8bf14671612f6660add45a2
Sarah Ivey <sarahivey1@yahoo.com>
2016-03-10 16:25:02 -0500

    gladiator trident wins

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,29 @@
+
 class Arena
+  attr_accessor :name
+  attr_accessor :gladiators
+
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators= []
+  end
+
+  def add_gladiator(gladiator)
+    if @gladiators.count < 2
+      @gladiators.push(gladiator)
+    end
+  end
+
+  def fight
+    return if @gladiators.length < 2
+
+    gladiator1 = gladiators.first.weapon
+    gladiator2 = gladiators.last.weapon
+    if gladiator1 == "Trident" && gladiator2 == "Spear"
+      @gladiators.pop
+      puts gladiator2 dies
+    else
+      @gladiators.shift
+    end
+  end
 end
