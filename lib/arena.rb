ce3f9436944e695061b7682e9524a83a6124b9ce
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-10 11:49:40 -0500

    better optimized conditional

--- lib/arena.rb
+++ lib/arena.rb
@@ -2,14 +2,16 @@
 require_relative 'gladiator'
 
 class Arena
-  attr_accessor :name, :gladiators
-  def initialize name, gladiators = []
+  attr_accessor :name, :gladiators, :fight_weapons
+  def initialize name, gladiators = [], fight_weapons = []
     @name = name.capitalize
     @gladiators = gladiators
+    @fight_weapons = fight_weapons
   end
   def add_gladiator(gladiator)
     if @gladiators.length < 2
       @gladiators << gladiator
+      @fight_weapons << gladiator.weapon
     end
   end
   def fight
@@ -18,34 +20,26 @@ class Arena
     end
   end
   def getWinner
-    if gladiators[0].weapon == 'Trident' && gladiators[1].weapon == 'Spear'
-      gladiators.pop
-      puts "#{gladiators[0].name} wins!"
-    elsif gladiators[0].weapon == 'Spear' && gladiators[1].weapon == 'Trident'
-      gladiators.shift
-      puts "#{gladiators[0].name} wins!"
-    elsif gladiators[0].weapon == 'Spear' && gladiators[1].weapon == 'Club'
-      gladiators.pop
-      puts "#{gladiators[0].name} wins!"
-    elsif gladiators[0].weapon == 'Club' && gladiators[1].weapon == 'Spear'
-      gladiators.shift
-      puts "#{gladiators[0].name} wins!"
-    elsif gladiators[0].weapon == 'Club' && gladiators[1].weapon == 'Trident'
-      gladiators.pop
-      puts "#{gladiators[0].name} wins!"
-    elsif gladiators[0].weapon == 'Trident' && gladiators[1].weapon == 'Club'
-      gladiators.shift
-      puts "#{gladiators[0].name} wins!"
-    elsif gladiators[0].weapon == gladiators[1].weapon
-      "Same weapon. #{gladiators[0].name} & #{gladiators[1].name} both die."
-      @gladiators = []
+    if fight_weapons.sort! == ["Spear","Trident"]
+      gladiators.delete_if {|gladiator| gladiator.weapon == "Spear"}
+      "#{gladiators[0].name} wins"
+    elsif fight_weapons.sort! == ["Club","Spear"]
+      gladiators.delete_if {|gladiator| gladiator.weapon == "Club"}
+      "#{gladiators[0].name} wins"
+    elsif fight_weapons.sort! == ["Club","Trident"]
+      gladiators.delete_if {|gladiator| gladiator.weapon == "Trident"}
+      "#{gladiators[0].name} wins"
+    else gladiators.clear
+      "Same weapon. Both die."
     end
   end
 end
-arena = Arena.new("Metropolis")
 
+#testing purposes:
+arena = Arena.new("Metropolis")
 max = Gladiator.new("Max","Trident")
 ren = Gladiator.new("Ren","Spear")
 arena.add_gladiator(max)
 arena.add_gladiator(ren)
+
 # binding.pry
