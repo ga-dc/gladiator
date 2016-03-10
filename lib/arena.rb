2d12bd10020792fea192f5f5daf56591ddbff3a8
Anissa Jones <anissajones10@gmail.com>
2016-03-10 11:46:28 -0500

    refactored add_gladiator after understanding .count method for arrays.

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,5 +1,5 @@
-require "pry"
-require_relative "gladiator"
+# require "pry"
+# require_relative "gladiator"
 
 class Arena
     attr_accessor :name, :gladiators, :count
@@ -7,13 +7,11 @@ class Arena
     def initialize(name)
         @name = name.capitalize
         @gladiators = []
-        @count = 0
     end
 
     def add_gladiator(glad)
-        if @count < 2
+        if @gladiators.count < 2
             @gladiators << glad
-            @count += 1
         end
     end
 
@@ -28,10 +26,10 @@ class Arena
 
 end
 
-binding.pry
-puts "bugfix"
+# binding.pry
+# puts "bugfix"
 # gladiatrix = Gladiator.new("Anissa", "Spear")
 # metro = Arena.new("Metro")
 # metro.add_gladiator(gladiatrix)
-# @gladiators => [gladiatrix]
-# gladiatrix
+# metro.gladiators => [gladiatrix]
+# metro.gladiators[0].weapon
