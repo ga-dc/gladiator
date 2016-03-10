5e6b8ca85e63cacf98cf9c7ecbd29ea4407d0b59
Hilary Masland <hilarymasland@gmail.com>
2016-03-10 11:40:14 -0500

    Gladiator tested. Beginning Arena tests

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,9 @@
 class Arena
+  attr_accessor :arena_name, :gladiators
+
+def initialize (arena_name, gladiators)
+  @arena_name = arena_name.capitalized
+  @gladiators = []
+end
+
 end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,19 +1,24 @@
 # A gladiator has a name
 # A gladiator has a weapon (one of Spear, Club, Trident)
-require "pry"
+# require "pry"
 
 class Gladiator
 
-  # attr_accessor = :gladiator
-  # attr_accessor = :weapon
+  attr_accessor :name, :weapon
 
 
   # def name
   #   if Gladiator.name("Maximus")
   #     return
-def initialize(initial_name, initial_weapon)
-  @name = initial_name
-  @weapon = initial_weapon
-
+def initialize(name, weapon)
+  @name = name
+  @weapon = weapon
+end
 
 end
+
+# Maximus = Gladiator.new("Maximus", "Spear")
+
+# binding.pry
+#
+# puts "done"
