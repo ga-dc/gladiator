37635a0bda4c778feb8395f9fc036bff5a61e346
Christine Movius <christine.movius@gmail.com>
2016-03-10 09:37:20 -0500

    weapons limited to club, spear, and trident. capitalizes attributes.

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,14 +1,25 @@
+# require "pry"
+
 class Gladiator
   attr_accessor :name, :weapon
 
   def initialize name, weapon
-    if name == 'Maximus'
-      @name = name
+    if name.capitalize == 'Maximus'
+      @name = name.capitalize
       @weapon = 'Sword'
+    elsif weapon.capitalize == 'Club' || weapon.capitalize == 'Spear' || weapon.capitalize == 'Trident'
+      @name = name.capitalize
+      @weapon = weapon.capitalize
     else
-      @name = name
-      @weapon = weapon
+      puts 'Not a valid weapon. Please choose Club, Spear, or Trident.'
     end
   end
 
 end
+
+chris = Gladiator.new("chris", "club")
+pat = Gladiator.new("pat", "spear")
+sanjay = Gladiator.new("sanjay", "trident")
+maximus = Gladiator.new("maximus", "club")
+
+# binding.pry
