b78d1b36fb369f45fb740bea1b790ad4fe0c35cb
Ryan Krolick <rkrolick@gmail.com>
2016-03-10 14:30:27 -0500

    added comments

--- lib/arena.rb
+++ lib/arena.rb
@@ -6,6 +6,7 @@ class Arena
     @gladiators = []
   end
 
+  # adds a gladiator to the arena.
   def add_gladiator(g)
     if gladiators.count < 2
       gladiators << g
@@ -25,6 +26,7 @@ class Arena
       return gladiators.pop
   end
 
+  # returns true if there are at least 2 gladiators in the arena.
   def is_able?
     if gladiators.count < 2
       return false
@@ -32,6 +34,8 @@ class Arena
     return true
   end
 
+  # returns true and removes gladiators from arena if two gladiators are using
+  # the same weapon.
   def is_tie?
     if gladiators[0].weapon == gladiators[1].weapon
       @gladiators = []
@@ -57,6 +61,7 @@ class Arena
    end
  end
 
+ # Should be used to put losing player at the top of the array.
  def swap_players
    temp = gladiators[0]
    @gladiators[0] = gladiators[1]
