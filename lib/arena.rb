d68bd9def60505ce2b69b5be1542c55de16d8ea9
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-10 14:50:54 -0500

    0 gladiators = nothing 1 gladiator = nothing

--- lib/arena.rb
+++ lib/arena.rb
@@ -12,9 +12,24 @@ class Arena
   def add_gladiator (name)
     if @gladiators.length < 2
      @gladiators << name
-   else "You have 2 Gladiators!"
+    else
+      "You have 2 Gladiators!"
+    end
   end
-end
+
+  def fight
+    # if gladiator =0 then action in arena = nil
+    if @gladiators.count == 0
+      return nil
+    end
+    # if gladiator =1 then action in arena =nil
+    if @gladiators.count == 1
+      return nil
+    end
+
+  end
 
 
+
+  
 end
