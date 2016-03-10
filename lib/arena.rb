2812612d1f8bbe7973d751183347a05b62210f73
Anissa Jones <anissajones10@gmail.com>
2016-03-10 10:44:36 -0500

    number of gladiators starts as nil.

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,8 +1,16 @@
 class Arena
-    attr_accessor :name
+    attr_accessor :name, :gladiators, :count
 
     def initialize(name)
         @name = name.capitalize
+        @gladiators = []
+        @count = 0
     end
 
+    def add_gladiator(glad)
+        if @count < 2
+            @new_glad = glad
+            @count += 1
+        end
+    end
 end
