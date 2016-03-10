34cb782a5d56e2e077a3fc2761b7d1b1c082f723
Cam <camlatimer@gmail.com>
2016-03-10 10:32:22 -0500

    started filling out Arena class

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,27 @@
+require "pry"
+
+# Arena has a name
+# name is always capitalized
+# gladiators starts empty
+# add_gladiator adds a gladiator to the arena
+# add_gladiator can add multiple gladiators
+# add_gladiator does not have more than two gladiators at once
+# fight when there are no gladiators does nothing
+# fight when there is one gladiator does nothing
+# fight when Spear v Trident kills: spear when fighting trident
+# fight when Club v Spear kills: club when fighting spear
+# fight when Trident v Club kills: trident when fighting club
+# fight when same Weapons kills: both if their weapons are the same
+
 class Arena
+  def initialize name
+    @name = name.upcase
+    @gladiators = []
+  end
+  def add_gladiator
+  end
+  def fight
+  end
 end
+
+binding.pry
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,4 +1,12 @@
-# Gladiator#name has a name
-rspec ./spec/gladiator_spec.rb:13 # Gladiator#name has a weapon
+require "pry"
+# Gladiator has a name
+# Glasdiator has a weopon
+rspec ./spec/gladiator_spec.rb:13 #name has a weapon
 class Gladiator
+  def initialize name, weapon
+    @name = name
+    @weapon = weapon
+  end
 end
+
+binding.pry
