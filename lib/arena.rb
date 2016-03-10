dc4e41e443966b0b8de4d11343045ad4fc769287
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-10 14:40:25 -0500

    saving before refactoring

--- lib/arena.rb
+++ lib/arena.rb
@@ -28,28 +28,31 @@ class Arena
     elsif @gladiators[1].name == "Maximus"
       @gladiators.delete_at(0)
       p "#{@gladiators[1].name} has won!"
-    else
-      puts "#{@gladiators[0].name} has a #{@gladiators[0].weapon} and is fighting #{@gladiators[1].name}, who has a #{@gladiators[1].weapon}."
-      # puts "#{1gladiator.name} has a #{1gladiator.weapon} and #{2gladiator.name} has a #{2gladiator.weapon}"
-      if ((@gladiators[0].weapon == "Spear") && (@gladiators[1].weapon == "Trident") ||
-        (@gladiators[0].weapon == "Club") && (@gladiators[1].weapon == "Spear") ||
-        (@gladiators[0].weapon == "Trident") && (@gladiators[1].weapon == "Club"))
-        remove_gladiator(@gladiators[0])
-        @gladiators.delete(@gladiators[0])
-      elsif ((@gladiators[1].weapon == "Spear") && (@gladiators[0].weapon == "Trident") ||
-        (@gladiators[1].weapon == "Club") && (@gladiators[0].weapon == "Spear") ||
-        (@gladiators[1].weapon == "Trident") && (@gladiators[0].weapon == "Club"))
-        @gladiators.delete(@gladiators[1])
-      elsif @gladiators[0].weapon == @gladiators[1].weapon
-        p "Both #{@gladiators[0].name} and #{@gladiators[1].name} have died!"
-        @gladiators = []
-      end
+    elsif @gladiators.count == 2
+      get_winner
     end
   end
 
-def remove_gladiator(gladiator)
-  p "#{gladiator.name} has died!"
-end
+  def get_winner
+    puts "#{@gladiators[0].name} has a #{@gladiators[0].weapon} and is fighting #{@gladiators[1].name}, who has a #{@gladiators[1].weapon}."
+    if ((@gladiators[0].weapon == "Spear") && (@gladiators[1].weapon == "Trident") ||
+      (@gladiators[0].weapon == "Club") && (@gladiators[1].weapon == "Spear") ||
+      (@gladiators[0].weapon == "Trident") && (@gladiators[1].weapon == "Club"))
+      remove_gladiator(@gladiators[0])
+      @gladiators.delete(@gladiators[0])
+    elsif ((@gladiators[1].weapon == "Spear") && (@gladiators[0].weapon == "Trident") ||
+      (@gladiators[1].weapon == "Club") && (@gladiators[0].weapon == "Spear") ||
+      (@gladiators[1].weapon == "Trident") && (@gladiators[0].weapon == "Club"))
+      @gladiators.delete(@gladiators[1])
+    elsif @gladiators[0].weapon == @gladiators[1].weapon
+      p "Both #{@gladiators[0].name} and #{@gladiators[1].name} have died!"
+      @gladiators = []
+    end
+  end
+
+  def remove_gladiator(gladiator)
+    p "#{gladiator.name} has died!"
+  end
 
 end
 
@@ -59,10 +62,6 @@ end
 # end
 
 
-# def get_winner
-#   @gladiators.any? { |weapon| gladiators.include?("Spear") }
-#   if @gladiators[0].weapon.include?("Spear") @gladiators[1]
-# end
 
 
 # arena = Arena.new("megalopolis")
