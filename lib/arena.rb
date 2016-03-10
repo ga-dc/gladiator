8e965aa75fa42abba13a5f398414c245e696f8ea
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-10 09:53:35 -0500

    built add gladiator method

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,10 +1,23 @@
+# require "pry"
+
+
 class Arena
-  attr_accessor :name
+  attr_accessor :name, :gladiators
   def initialize(name)
     @name = name.capitalize
+    @gladiators = []
+  end
+
+  def add_gladiator(gladiator)
+    if @gladiators.count < 2
+      @gladiators << gladiator
+    end
+
   end
 
 
 
-
 end
+
+# binding.pry
+# puts "ayyy"
