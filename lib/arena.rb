8931825158478d4fd01f41f7f6616128fd35c08d
Christine Movius <christine.movius@gmail.com>
2016-03-10 11:08:42 -0500

    gladiators in arena are sorted alphabetically by weapon.

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,3 +1,6 @@
+require_relative "gladiator"
+require "pry"
+
 class Arena
   attr_accessor :name, :gladiators
 
@@ -9,7 +12,10 @@ class Arena
   def add_gladiator gladiator
     if @gladiators.length < 2
       @gladiators << gladiator
-      puts @gladiators
+      @gladiators.sort_by! do |gladiator|
+        gladiator.weapon
+      end
+      @gladiators
     else
       puts "There are 2 gladiators in the arena already."
     end
@@ -19,10 +25,19 @@ class Arena
     if @gladiators.length <= 1
       puts "Needs more gladiators!"
     else
-      gladiators.each do
-        puts gladiators.name
+      gladiators.each do |gladiator|
+        if @gladiators[0].weapon == "Spear" && @gladiators[1].weapon = "Trident"
+          @gladiators.shift
+        end
       end
     end
   end
 
 end
+
+adrian = Gladiator.new("adrian", "club")
+robin = Gladiator.new("robin", "spear")
+
+dupont = Arena.new("dupont")
+
+binding.pry
