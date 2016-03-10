b28ff8c2b0056816489b9b70ddb734af3d768fbe
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-10 10:15:53 -0500

    initial commit

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,9 @@
 class Arena
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
 end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,12 @@
 class Gladiator
+
+  attr_accessor :name, :weapon
+
+  def initialize(name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+
 end
+
+maximus = Gladiator.new("Maximus", "Sword")
