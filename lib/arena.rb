a8df07242298a426736c9015967c698bdd68a26d
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-03-10 10:28:09 -0500

    Initial commit

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,37 @@
+require 'pry'
+
 class Arena
+
+  attr_accessor :name, :gladiators
+
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+
+  def add_gladiator(gladiator)
+    if @gladiators.length < 2
+      @gladiators << gladiator
+    end
+  end
+
+  def fight
+    if @gladiators.length == 2
+      if @gladiators.include?('Trident') and @gladiators.include?('Spear')
+        if @gladiator[0].include?('Trident')
+          @gladiator.delete_at(1)
+
+          puts "Hello"
+        else
+          @gladiator.delete_at(0)
+        end
+      end
+    end
+  end
+
 end
+# 
+# maximus = Gladiator.new("Maximus","Spear")
+# bilcephalon = Gladiator.new("Bilcephalon","Trident")
+
+binding.pry
