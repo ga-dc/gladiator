ab4d3a55fe89358bfc913755ca2f182e4e920b26
Christine Movius <christine.movius@gmail.com>
2016-03-10 09:27:04 -0500

    Intial Commit. Gladiator spec passes, need to limit weapons to club, spear, and trident and sword only for Maximus.

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,14 @@
 class Gladiator
+  attr_accessor :name, :weapon
+
+  def initialize name, weapon
+    if name == 'Maximus'
+      @name = name
+      @weapon = 'Sword'
+    else
+      @name = name
+      @weapon = weapon
+    end
+  end
+
 end
