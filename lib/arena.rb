242e7d60fa0001422eeed540f39d47cd5e91b4a0
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-11 08:39:02 -0500

    Need to fix for more rspec failures

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,3 +1,5 @@
+# require_relative "..lib/gladiator.rb"
+
 class Arena
   attr_accessor :name, :gladiators
 
@@ -6,20 +8,38 @@ class Arena
     @gladiators = gladiators
   end
 
-def add_gladiator new_gladiator_name
-  if @gladiators.count ==2
-    puts "You cannot have more than two Gladiators in the arena"
-  else
-  @gladiators << new_gladiator_name
-puts "#{@gladiators.count}"
-end
+  def add_gladiator new_gladiator_name
+    if @gladiators.count ==2
+      puts "You cannot have more than two Gladiators in the arena"
+    else
+      @gladiators << new_gladiator_name
+      puts "#{@gladiators.count}"
+    end
+  end
+
+  def fight
+    if @gladiators.count == 2
+      @gladiatorsS
+      @gladiators.reverse!
+
+    end
+  end
+
 end
 
-def fight
-if @gladiators.count == 0
-  puts "You have no Gladiator in the arena"
-end
-end
-
-
-end
+# if @gladiators.
+# def choose(decide_winner)
+#   decide_winner = []
+#
+# if decide_winner = ["Trident", "Spear"]
+#   return winner = "Trident"
+# elsif decide_winner =  ["Spear", "Club"]
+#   return "Spear"
+# elsif decide_winner =  ["Club", "Triedent"]
+#   return winner = "Club"
+# elsif decide_winner =  ["Club", "Triedent"]
+#   return winner = "Club"
+# else
+#    return puts "Draw"
+# end
+# end
