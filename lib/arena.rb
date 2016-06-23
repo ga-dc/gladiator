eba00aaf546ba746e5f59342a84bcdd02d606510
Adina Pollack <adinap16@gmail.com>
2016-06-23 11:24:41 -0400

    Pass all tests

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,36 @@
+require_relative 'gladiator'
+require 'gladiator'
+
 class Arena
+  attr_accessor :name, :gladiators
+  def initialize (name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+  def add_gladiator (name)
+    if @gladiators.length < 2
+      @gladiators << (name)
+    else
+    end
+  end
+  def fight
+    if @gladiators.length > 1
+      if @gladiators[0].weapon == 'Trident' && @gladiators[1].weapon == 'Spear'
+        @gladiators.delete_at(1)
+      elsif @gladiators[1].weapon == 'Trident' && @gladiators[0].weapon == 'Spear'
+        @gladiators.delete_at(0)
+      elsif @gladiators[0].weapon == 'Spear' && @gladiators[1].weapon == 'Club'
+        @gladiators.delete_at(1)
+      elsif @gladiators[1].weapon == 'Spear' && @gladiators[0].weapon == 'Club'
+        @gladiators.delete_at(0)
+      elsif @gladiators[0].weapon == 'Club' && @gladiators[1].weapon == 'Trident'
+        @gladiators.delete_at(1)
+      elsif @gladiators[1].weapon == 'Club' && @gladiators[0].weapon == 'Trident'
+        @gladiators.delete_at(0)
+      elsif @gladiators[0].weapon == @gladiators[1].weapon
+        @gladiators = []
+      end
+    else
+    end
+  end
 end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,10 @@
+require_relative 'arena'
+require 'arena'
+
 class Gladiator
+  attr_accessor :name, :weapon
+  def initialize (name, weapon)
+    @name = name
+    @weapon = weapon
+  end
 end
