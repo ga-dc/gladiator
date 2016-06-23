64ffec74c78e949050e32fd1156e739883abc882
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-06-23 11:41:15 -0400

    Add examples to gladiator spec

--- spec/gladiator_spec.rb
+++ spec/gladiator_spec.rb
@@ -6,3 +6,20 @@ require_relative '../lib/arena'
 A gladiator has a name
 A gladiator has a weapon (one of Spear, Club, Trident)
 =end
+
+describe Gladiator do
+
+  let(:masatoshi) { Gladiator.new("Masatoshi", "Spear") }
+
+  it "has a name" do
+    expect(masatoshi.name).to_not be_nil
+    expect(masatoshi.name).to eq("Masatoshi")
+  end
+
+  it "has a weapon (one of Spear, Club, Trident)" do
+    expect(masatoshi.weapon).to_not be_nil
+    expect(masatoshi.weapon).to include("Spear")
+    weapons = %w(Spear Club Trident)
+    expect(weapons).to include(masatoshi.weapon)
+  end
+end
