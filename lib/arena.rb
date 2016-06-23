78d5f3dc263abc2356f851de0ba1756632ed69aa
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-23 14:49:48 -0400

    Two more left

--- lib/arena.rb
+++ lib/arena.rb
@@ -19,21 +19,21 @@ class Arena
      @gladiators[0]
      @gladiators[1]
      if @gladiators[0].weapon == 'Trident'&& @gladiators[1].weapon != 'Club'
-         @gladiators.delete_at 1
+         @gladiators.delete_at(1)
        end
     if @gladiators[0].weapon == 'Spear'&& @gladiators[1].weapon != 'Trident'
-          @gladiators[1].delete_at(1)
+          @gladiators.delete_at(1)
         end
     if @gladiators[0].weapon == 'Club'&& @gladiators[1].weapon != 'Spear'
-             @gladiators[1].pop(1)
+             @gladiators.delete_at(1)
           end
 
   if @gladiators[0].weapon == @gladiators[1].weapon
-           @gladiators[0].pop(0)
-           @gladiators[1].pop(1)
+           @gladiators.delete_at(0)
+           @gladiators.delete_at(1)
 
        else
-         @gladiators[0].pop(0)
+         @gladiators.delete_at(0)
        end
      end
   end
