b2c48a18b02cb0afb0593bcb0470b8751c7c78ea
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2016-06-23 17:45:36 -0400

    mvp achieved =)

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,77 @@
 class Arena
-end
+  attr_accessor :name, :gladiators
+
+  def initialize(name)
+    @name = name.capitalize
+    @gladiators = []
+  end
+  def add_gladiator(glad)
+    if @gladiators.length < 2
+      @gladiators << glad
+    end
+  end
+
+  def fight
+
+        size = @gladiators.length
+
+        if size < 2
+          puts "nothing"
+
+        elsif size == 2
+          if  @gladiators[0].weapon == @gladiators[1].weapon
+            @gladiators = []
+
+          elsif @gladiators[0].weapon == "Spear"
+            if @gladiators[1].weapon == "Trident"
+              @gladiators.delete_at(0)
+            else
+              @gladiators.delete_at(1)
+            end
+
+          elsif @gladiators[0].weapon == "Trident"
+            if @gladiators[1].weapon == "Club"
+              @gladiators.delete_at(0)
+            else
+              @gladiators.delete_at(1)
+            end
+
+          elsif @gladiators[0].weapon == "Club"
+            if @gladiators[1].weapon == "Spear"
+              @gladiators.delete_at(0)
+            else
+              @gladiators.delete_at(1)
+            end
+          end
+        end
+      end
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # dave = Gladiator.new("Dave","Chainsaw")
+    # # dave = {name: "Dave",weapon: "Chainsaw"}
+    # myArena = Arena.new("denzel")
+    # # myArena = {name: "denzel"}
+    # denzel.add_gladiator(dave)
+    # # @gladiators = [dave]
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,19 @@
 class Gladiator
+  attr_accessor :name, :weapon
+  def initialize(name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+    # def createGladiators
+    # names = ["Maximus", "Bilcephalon","Ephates","Cylodeus"]
+    # weapons = ["Spear","Bilcephalon","Club","Club"]
+    # arenaEntrance =[]
+    # gladiators=[]
+    # names.each do |name|
+    #  weapons.each do |weapon|
+    #    puts arenaEntrance << [name,weapon]
+    #    arenaEntrance.each_slice(2) << [gladiators]
+    #    return gladiators
+    #  end
+
 end
