36f1d1296469e20440f6f4d2f2da663582e8acc3
Adina Pollack <adinap16@gmail.com>
2016-06-23 16:14:56 -0400

    made some edits to fight method

--- models/arena.rb
+++ models/arena.rb
@@ -6,24 +6,19 @@ require_relative "gladiator"
 
 class Arena < ActiveRecord::Base
   has_many :gladiators
-  def add_gladiator (name)
-    if @gladiators.length < 2
-      @gladiators << (name)
+  @gladiators = [Gladiator.first, Gladiator.last]
+  def fight
+    if @gladiators.length > 1
+      if @gladiators[0].weapon == @gladiators[1].weapon
+        @gladiators = []
+      elsif @gladiators[0].weapon == 'Trident' && @gladiators[1].weapon == 'Spear' ||
+        @gladiators[0].weapon == 'Spear' && @gladiators[1].weapon == 'Club' ||
+        @gladiators[0].weapon == 'Club' && @gladiators[1].weapon == 'Trident'
+        @gladiators.delete_at(1)
+      else
+        @gladiators.delete_at(0)
+      end
     else
     end
   end
-  # def fight
-  #   if @gladiators.length > 1
-  #     if @gladiators[0].weapon == @gladiators[1].weapon
-  #       @gladiators = []
-  #     elsif @gladiators[0].weapon == 'Trident' && @gladiators[1].weapon == 'Spear' ||
-  #       @gladiators[0].weapon == 'Spear' && @gladiators[1].weapon == 'Club' ||
-  #       @gladiators[0].weapon == 'Club' && @gladiators[1].weapon == 'Trident'
-  #       @gladiators.delete_at(1)
-  #     else
-  #       @gladiators.delete_at(0)
-  #     end
-  #   else
-  #   end
-  # end
 end
