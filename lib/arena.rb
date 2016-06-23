58ae74354bee2c7e4d35431fe69b02244a95ce21
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-23 16:21:48 -0400

    14 tests passing

--- lib/arena.rb
+++ lib/arena.rb
@@ -10,6 +10,7 @@ class Arena < Gladiator
   def initialize(name)
     @name = name.capitalize
     @gladiators = []
+    @counter = 0
   end
 
 # 1 Arena name
@@ -18,13 +19,11 @@ class Arena < Gladiator
     @name
   end
 
+# 4 add_gladiator adds a gladiator to the arena
+# 5 gladiator can add multiple gladiators to the arena
+# 6 but not more than two at the same time
 
-# 7 fight when no gladiators does nothing
-# 8 fight when one gladiator does nothing
 
-#9 when Spear v Trident kills: spear when fighting trident
-#10 when Club v Spear kills: club when fighting spear
-#11 when Trident v Club kills: trident when fighting club
 
 def add_gladiator(gladiator)
   if @gladiators.length == 2
@@ -34,16 +33,56 @@ def add_gladiator(gladiator)
   end
 end
 
+# 7 fight when no gladiators does nothing
+# 8 fight when one gladiator does nothing
+
+#9 when Spear v Trident kills: spear when fighting trident
+#10 when Club v Spear kills: club when fighting spear
+#11 when Trident v Club kills: trident when fighting club
+
+#
+
+def fight
+  if @gladiators.length == 2
+    # puts "#{@gladiators}"
+    @gladiators.each do |type|
+      weapons_rank = ["Spear", "Club", "Trident"]
+      w = type.weapon
+      @counter = @counter + weapons_rank.index(w)
+    end
+
+    if @gladiators[0].weapon == @gladiators[1].weapon
+      @gladiators = []
+    end
+
+    if @counter == 1 && @gladiators[0].weapon == "Spear" #spear and club, club wins
+      @gladiators.delete(@gladiators[1])
+      return
+    else
+      @gladiators.delete(@gladiators[0])
+      puts "#{@gladiators}"
+      return
+    end
 
 
-  def fight
-    # if @gladiators.length == 2
-    #   puts 1
-    # end
+    if @counter == 2 && @gladiators[0].weapon == "Spear" #spear and trident, trident wins
+      @gladiators.delete(@gladiators[0])
+    else
+      @gladiators.delete(@gladiators[1])
+      puts "#{@gladiators}"
+    end
+
+    if @counter == 2 && @gladiators[0].weapon == "Trident" #club and trident, club wins
+      @gladiators.delete(@gladiators[0])
+    else
+      @gladiators.delete(@gladiators[1])
+      puts "#{@gladiators}"
+    end
+
   end
-
-
 end
+
+end  #class end
 
 
 
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -4,32 +4,12 @@ class Gladiator
   attr_accessor :name, :weapon
 
 
- # 4 add_gladiator adds a gladiator to the arena
- # 5 gladiator can add multiple gladiators to the arena
- #6 but not more than two at the same time
-
+# gladiator has a name and a weapon
 
   def initialize(name, weapon)
     @name = name
     @weapon = weapon
   end
-
-
-  #
-  #
-  # def add_gladiator
-  #   loop do |add|
-  #     # puts "Enter a gladiator! #{@gladiators.length} entered so far:"
-  #     gladiator = gets.chomp
-  #     @gladiators << gladiator
-  #
-  #     if @gladiators.length == 2
-  #       break
-  #     end
-  #   end
-  # end
-
-
 end
 
 
