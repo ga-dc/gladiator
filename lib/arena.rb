837dad6a3b70eba0be02d3c2ffc4257029f75f88
Adina Pollack <adinap16@gmail.com>
2016-06-23 13:46:50 -0400

    cleaned up code

--- lib/arena.rb
+++ lib/arena.rb
@@ -15,20 +15,14 @@ class Arena
   end
   def fight
     if @gladiators.length > 1
-      if @gladiators[0].weapon == 'Trident' && @gladiators[1].weapon == 'Spear'
-        @gladiators.delete_at(1)
-      elsif @gladiators[1].weapon == 'Trident' && @gladiators[0].weapon == 'Spear'
-        @gladiators.delete_at(0)
-      elsif @gladiators[0].weapon == 'Spear' && @gladiators[1].weapon == 'Club'
-        @gladiators.delete_at(1)
-      elsif @gladiators[1].weapon == 'Spear' && @gladiators[0].weapon == 'Club'
-        @gladiators.delete_at(0)
-      elsif @gladiators[0].weapon == 'Club' && @gladiators[1].weapon == 'Trident'
-        @gladiators.delete_at(1)
-      elsif @gladiators[1].weapon == 'Club' && @gladiators[0].weapon == 'Trident'
-        @gladiators.delete_at(0)
-      elsif @gladiators[0].weapon == @gladiators[1].weapon
+      if @gladiators[0].weapon == @gladiators[1].weapon
         @gladiators = []
+      elsif @gladiators[0].weapon == 'Trident' && @gladiators[1].weapon == 'Spear' ||
+        @gladiators[0].weapon == 'Spear' && @gladiators[1].weapon == 'Club' ||
+        @gladiators[0].weapon == 'Club' && @gladiators[1].weapon == 'Trident'
+        @gladiators.delete_at(1)
+      else
+        @gladiators.delete_at(0)
       end
     else
     end
