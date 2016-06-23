1af3cae256035b73d2ced26cc626605ad0d136b1
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-23 09:26:01 -0400

    tests 1 and 2 passed

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,18 @@
 class Arena
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name.capitalize
+  end
+
+# 1 Arena name
+# 2 Arena name is always capitalized
+  def arena_name
+    puts "#{@name}"
+  end
+
 end
+
+
+
+# m = Arena.new("megalopolis")
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,23 @@
 class Gladiator
+  attr_accessor :gladiators
+
+ # 3 gladiators starts off empty
+ # 4 add_gladiator adds a gladiator to the arena
+ # 5 gladiator can add multiple gladiators to the arena
+ #6 but not more than two at the same time
+  def initialize()
+    @gladiators = []
+  end
+
+  def add_gladiator(gladiator)
+    @gladiators << gladiator
+  end
+
+
+# 7 fight when no gladiators does nothing
+
+
 end
+
+
+# m = Gladiator.new("Maximus")
