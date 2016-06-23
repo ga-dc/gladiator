61ac81a4d452d08b78bf2e5d204caf999295cfdd
Kevin Golden <kwg@cs.net>
2016-06-23 14:58:01 -0400

    ugly code but no rspec messages

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,15 @@
+# require 'pry'
+require_relative './arena'
 class Gladiator
+  attr_accessor :name, :weapon
+
+  def initialize(in_name, in_weapon)
+    @name =  in_name.capitalize
+    @weapon = in_weapon
+  end
+  # def name
+  #   return @name
+  # end
 end
+
+# binding.pry
