901737f3afd9421697743df5c160a983a22396f2
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-06-23 12:09:31 -0400

    Fill arena spec with pending examples

--- spec/arena_spec.rb
+++ spec/arena_spec.rb
@@ -15,3 +15,56 @@ If there are two gladiators in the arena, you can call a fight method that resul
     Club beats Trident
     If the two gladiators have the same weapon, they are both eliminated.
 =end
+
+describe Gladiator do
+  it "has a name that is capitalized"
+  it "can have gladiators"
+
+  describe "#add_gladiator" do
+    context "when it has 0..1 gladiator" do
+      it "can add gladiators"
+    end
+    context "when it has 2 gladiators" do
+      it "cannot add gladiators"
+    end
+  end
+
+  describe "#fight" do
+    context "when it has 0..1 gladiator" do
+      it "does nothing on gladiators"
+    end
+    context "when it has 2 gladiators" do
+      it "removes loser(s) from the list"
+    end
+  end
+
+  describe "#determine_winner" do
+    context "when the two gladiators have the same weapon" do
+      it "returns nil"
+    end
+    context "if spear vs" do
+      context "club" do
+        it "spear wins"
+      end
+      context "trident" do
+        it "trident wins"
+      end
+    end
+    context "if club vs" do
+      context "trident" do
+        it "club wins"
+      end
+      context "spear" do
+        it "spear wins"
+      end
+    end
+    context "if trident vs" do
+      context "spear" do
+        it "trident wins"
+      end
+      context "club" do
+        it "club wins"
+      end
+    end
+  end
+end
