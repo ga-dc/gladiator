7e863d2f7e182e2485f0e8709f754b293dffe525
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-23 13:23:56 -0400

    Almost done

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,6 +1,43 @@
 class Arena
-  attr_accessor :name
+
+  attr_accessor :name, :gladiators
   def initialize(name)
-    @name = name
+    @name = name.capitalize
+    @gladiators = []
   end
+
+  def add_gladiator(name)
+    if @gladiators.count < 2
+    @gladiators<< name
+  else
+    puts "Only two gladiators allowed per Arena"
+    end
+  end
+
+  def fight
+   if @gladiators.count == 2
+     @gladiators[0]
+     @gladiators[1]
+     if @gladiators[0].weapon == 'Trident'&& @gladiators[1].weapon != 'Club'
+         @gladiators.delete_at 1
+       end
+    if @gladiators[0].weapon == 'Spear'&& @gladiators[1].weapon != 'Trident'
+          @gladiators[1].delete_at(1)
+        end
+    if @gladiators[0].weapon == 'Club'&& @gladiators[1].weapon != 'Spear'
+             @gladiators[1].pop(1)
+          end
+
+  if @gladiators[0].weapon == @gladiators[1].weapon
+           @gladiators[0].pop(0)
+           @gladiators[1].pop(1)
+
+       else
+         @gladiators[0].pop(0)
+       end
+     end
+  end
+
+
+
 end
