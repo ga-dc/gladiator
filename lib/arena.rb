80278aca33d87f9aefe005a48586ae85b353a593
Travis Batts <Travisbatts6@gmail.com>
2016-10-27 14:27:08 -0400

    lab

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,2 +1,35 @@
 class Arena
+
+  def initialize (name)
+   @name = name
+   @gladiators = []
+  end
+ def name
+  return @name.capitalize
+ end
+ def gladiator
+   return @gladiators
 end
+ def add_gladiator(new_gladiator)
+   if @gladiators.count < 2
+     @gladiators.push(new_gladiator)
+  # if @gladiators.count > 2
+  #    break
+  #  end
+   end
+ end
+def fight
+  weapon_switch = {"Trident" => "Spear", "Spear" =>"Club", "Club" => "Trident" }
+    if @gladiators.count ==2
+      if @gladiators.first.weapon_switch == "Trident"
+        if @gladiators.first.weapon_switch == "Spear"
+
+# def fight
+#   weapon_switch = {"Trident" => "Spear", "Spear" =>"Club", "Club" => "Trident" }
+#
+#   if @gladiators.count == 2
+#     if @gladiators.first.weapon_switch == "Trident"
+#     end
+#    end
+#  end
+# end
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,12 @@
 class Gladiator
+  def initialize name, weapon
+    @name = name
+    @weapon = weapon
+end
+  def name
+    return name
+  end
+  def weapon
+    return weapon
+  end
 end
