abbbdd7c48e5aa476a0a6ab8afb07ae0449cd1e6
Donovan Allen <dallen.928@gmail.com>
2016-10-27 12:39:44 -0400

    initial commit

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,11 @@
+# require "pry"
+
 class Gladiator
+  attr_accessor :name, :weapon
+  def initialize(name, weapon) #spear, trident, club
+    @name = name.capitalize
+    @weapon = weapon.capitalize
+  end
 end
+
+# puts "end of file"
