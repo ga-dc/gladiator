487182e3ee42ee0a8d480411581b0e061874e514
EJ Sorrento <ejsorrento@gmail.com>
2016-10-27 23:23:05 -0400

    second try gladiator

--- lib/arena.rb
+++ lib/arena.rb
@@ -1,11 +1,39 @@
+#Practice
+require 'gladiator'
+# require "pry"
+
 class Arena
   attr_accessor :name
   def initialize name, gladiators = []
     @name = name.capitalize
-
+    @gladiators = gladiators
   end
+
+def add_gladiator(gladiator)
+   @gladiators.push(gladiator)
+   if @gladiators.length == 3
+     @gladiators.pop  #take one out
+   end
 end
 
-def name
-   @name = name.upcase!
-end
+def fight()
+  if @gladiators.length == 2
+    if @gladiators.first.weapon == "Spear" && @gladiators.last.weapon == "Trident"
+      @gladiator.delete_at(1)
+
+    elsif @gladiators.first.weapon == "Club" && @gladiators.last.weapon == "Spear"
+      @gladiators.delete_at(1)
+
+    elsif @gladiators.first.weapon == "Trident" && @gladiators.last.weapon == "Club"
+    @gladiators.delete_at(1)
+
+  else @gladiators.first.weapon == @gladiators.last.weapon
+    @gladiators.pop(2)
+
+    end
+
+
+
+
+# binding.pry
+# puts "oh hamburgers"
--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,15 @@
+#Practice
+
 class Gladiator
+  attr_accessor :name, :weapon
+  def initialize(name, weapon)
+    @name = name
+    @weapon = weapon
+  end
+
+
 end
+
+
+#gladiator = Gladiator.new("Maximus", "Sword")
+#above statement not necessary because of gladiator_spec.rb
