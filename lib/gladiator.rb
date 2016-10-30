c34be4fd856c6862e6ebf9f678d68e76f0268164
AdrienneD <adriennedao@gmail.com>
2016-10-29 20:50:58 -0400

    initial commit

--- lib/gladiator.rb
+++ lib/gladiator.rb
@@ -1,2 +1,18 @@
 class Gladiator
+
+  attr_accessor :name, :weapon
+
+  def initialize name, weapon
+    @name = name
+    @weapon = weapon
+
+  end
+
+  def name
+    @name
+end
+  def weapon
+    @weapon
+end
+
 end
